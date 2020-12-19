67ccfc923082079d29f231e46c4921da3ef60ead
AfifahKhoeriah <afififahkhoeriah@gmail.com>
2020-12-19 10:00:30 +0700

    Tugas 1 konversi dalam detik belum selesai

---
+++ tugas_python_dasar.py
@@ -0,0 +1,29 @@
+"""
+APLIKASI PARKIR SEDERHANA
+
+input = JAM BERAPA MASUK PARKIRAN (1-12)
+        PAGI ATAU MALAM?
+        JAM BERAPA KELUAR PARKIRAN (1-12)
+        PAGI ATAU MALAM?
+output = ANDA PARKIR SELAMA ... JAM, Anda harus bayar Rp ...
+HARGA = 3 jam pertama 3000/jam
+        setelah 3 jam pertama 1000/jam
+        biaya maksimal 25000
+"""
+
+"""
+APLIKASI CONVERTER HARI DAN DETIK
+
+input
+Mau Menghitung apa? Hari atau Detik?
+
+output:
+361 hari = 1 tahun 0 bulan 1 hari
+
+Ketentuan:
+1 tahun = 360 hari
+1 bulan = 30 hari
+1 jam = 3600 detik
+1 menit = 60 detik
+
+"""
